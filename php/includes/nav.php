8a26d79d76e7081de8c2f05bab0ea1573e1f7384
alladinMelico <melico.alladin@gmail.com>
2019-11-08 14:58:43 +0800

    change to searc icon

--- php/includes/nav.php
+++ php/includes/nav.php
@@ -31,7 +31,9 @@
 
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">
+        <img src="..\icons\png\magnifying-glass-8x.png" alt="">
+      </button>
     </form>
 
 
